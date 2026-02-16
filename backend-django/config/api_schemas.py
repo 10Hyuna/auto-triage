d08b1984904e1d19c4e00d71941e772ee116a418
10Hyuna <kimhaa1005@naver.com>
2026-02-17 00:22:20 +0900

    chore: env 설정 및 API 에러 스키마 파일 추가

---
+++ backend-django/config/api_schemas.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+
+class ErrorResponseSerializer(serializers.Serializer):
+    code = serializers.CharField()
+    message = serializers.CharField()
+    details = serializers.DictField(required=False)
